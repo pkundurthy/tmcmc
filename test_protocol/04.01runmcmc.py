0542de23a422d7e64b4ae1040216db70d4a6736f
Praveen Kundurthy <pkundurthy@astro.washington.edu>
2011-10-12 10:34:50 -0700

    04.01 protocol modification

--- test_protocol/04.01runmcmc.py
+++ test_protocol/04.01runmcmc.py
@@ -10,19 +10,23 @@ ModelParams = tmcmc.iomcmc.ReadStartParams('STARTPARAMS.TEST.data')
 BoundParams = tmcmc.iomcmc.ReadBoundFile('BOUNDS.TEST.data')
     
 ##SHIFT starting point
-#ShiftFactor = 15e0
-##ShiftFactor = -15e0
+#ShiftFactor = 15e0   #moves the starting point by 15xstep-size (~15sigma)
 #for Param in ModelParams.keys():
     #if ModelParams[Param]['open']:
         #ModelParams[Param]['value'] +=\
         #ShiftFactor*ModelParams[Param]['step']
 
+##Scale starting steps
+#ScaleFactor = 0.1   #starting step sizes decreased by a factor of 10
+#for Param in ModelParams.keys():
+    #if ModelParams[Param]['open']:
+        #ModelParams[Param]['step'] *= ScaleFactor
 
 Nsteps = 2e6
 func = 'MTQ_multidepth_tduration'
 
 tmcmc.mcmc.mcmc_mh_adapt(Nsteps,func,\
-    ObservedData,ModelParams,NuisanceData,\
-    BoundParams,False,True,\
-    'MCMC.TEST.mcmc',\
-    True)
+                         ObservedData,ModelParams,NuisanceData,\
+                         BoundParams,False,True,\
+                         'MCMC.TEST.mcmc',\
+                         True)
