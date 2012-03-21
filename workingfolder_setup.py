3fe714446aca39e53d07ee7cd81a21650625095f
Praveen Kundurthy <pkundurthy@astro.washington.edu>
2012-03-21 12:44:23 -0700

    update path

--- workingfolder_setup.py
+++ workingfolder_setup.py
@@ -14,14 +14,14 @@ def get_user_exec():
                         "FigurePath = MainPath+\'OtherFigures/\'",\
                         "PaperFiguresPath = MainPath+\'PaperFigures/\'"]
     elif user.lower() == 'acbecker':
-        ExecStatements =["MainPath = \'/astro/store/student-scratch1/pkundurthy/final_apostle/\'",\
+        ExecStatements =["MainPath = \'/astro/users/acbecker/APOSTLE/runs/WASP2_new/\'",\
                         "PicklePath = MainPath+'PickleStuff/\'",\
                         "ObjectList = [\'WASP2\',\'XO2\',\'TRES3\',\'GJ1214\']",\
                         "DataPrepPath = MainPath+\'DataPrep/\'",\
                         "FigurePath = MainPath+\'OtherFigures/\'",\
                         "PaperFiguresPath = MainPath+\'PaperFigures/\'"]
     else:
-        raise NameError("Abort!! Abort!! Unauthorized user %s detected" % user)
+        raise NameError("Unknown user %s detected" % user)
     
 
     return ExecStatements
